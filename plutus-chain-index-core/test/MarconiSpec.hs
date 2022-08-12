a0f19fdc28d349a29473d9270c0605b4fb98ce56
Markus Läll <markus.l2ll@gmail.com>
2022-08-12 19:20:35 +0300

    fixup! Add test for Marconi script transaction indexer

--- plutus-chain-index-core/test/MarconiSpec.hs
+++ plutus-chain-index-core/test/MarconiSpec.hs
@@ -11,10 +11,14 @@ module MarconiSpec where
 -- of the function's output.
 
 import Hedgehog
--- import Hedgehog.Gen qualified as Gen
--- import Hedgehog.Range qualified as Range
+import Hedgehog.Gen qualified as Gen
+import Hedgehog.Range qualified as Range
+
 import Test.Tasty (TestTree, testGroup)
 -- import Test.Tasty.Hedgehog (testPropertyNamed)
+import Cardano.Api
+import Control.Monad (replicateM)
+import Gen.Cardano.Api.Typed
 import Test.Tasty.Hedgehog (testProperty)
 
 import Marconi.Index.ScriptTx ()
@@ -24,4 +28,25 @@ tests = testGroup "marconi"
   [ testProperty "prop_script_hashes_in_tx_match" testTxScripts]
 
 testTxScripts :: Property
-testTxScripts = undefined
+testTxScripts = property $ do
+  -- Generate a random number of plutus scripts (see `genPlutusScript` from Gen.Cardano.Api.Typed)
+  nScripts <- forAll $ Gen.integral (Range.linear 0 500)
+  scripts :: [PlutusScript lang] <- replicateM nScripts $ forAll $ genPlutusScript undefined
+  -- data PlutusScript lang where
+  -- ^ /cardano-node/cardano-api/src/Cardano/Api/Script.hs::1061
+
+  -- then generate an TxIns which spends funds from these scripts
+  txIns <- undefined
+  -- type TxIns build era = [(TxIn, BuildTxWith build (Witness WitCtxTxIn era))]
+  -- ^ /cardano-node/cardano-api/src/Cardano/Api/TxBody.hs::1157
+
+  -- then generate a transaction with this TxIns (based on `genTxBodyContent`)
+  -- genTxBodyContent
+  -- data TxBodyContent build era =
+  -- ^ /cardano-node/cardano-api/src/Cardano/Api/TxBody.hs::1518
+
+  -- run your txScripts on this generated tx, and verify that the
+  -- initial generated plutus scripts are part of the function's
+  -- output
+
+  undefined
