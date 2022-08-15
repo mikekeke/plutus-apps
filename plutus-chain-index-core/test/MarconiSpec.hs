82521876399abd08b20c60d436a4516bc48c650e
Markus Läll <markus.l2ll@gmail.com>
2022-08-15 19:33:48 +0300

    wip

--- plutus-chain-index-core/test/MarconiSpec.hs
+++ plutus-chain-index-core/test/MarconiSpec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module MarconiSpec where
 
 -- Regarding property testing, here's a starting point.
@@ -17,8 +19,9 @@ import Hedgehog.Range qualified as Range
 import Test.Tasty (TestTree, testGroup)
 -- import Test.Tasty.Hedgehog (testPropertyNamed)
 import Cardano.Api
+import Cardano.Api qualified as Api
 import Control.Monad (replicateM)
-import Gen.Cardano.Api.Typed
+import Gen.Cardano.Api.Typed qualified as CGen
 import Test.Tasty.Hedgehog (testProperty)
 
 import Marconi.Index.ScriptTx ()
@@ -31,16 +34,27 @@ testTxScripts :: Property
 testTxScripts = property $ do
   -- Generate a random number of plutus scripts (see `genPlutusScript` from Gen.Cardano.Api.Typed)
   nScripts <- forAll $ Gen.integral (Range.linear 0 500)
-  scripts :: [PlutusScript lang] <- replicateM nScripts $ forAll $ genPlutusScript undefined
+  let lang = undefined :: lang
+  scripts :: [PlutusScript lang] <- replicateM nScripts $ forAll $ CGen.genPlutusScript lang
+  let scripts' = map (PlutusScript lang) scripts :: [Script lang]
   -- data PlutusScript lang where
   -- ^ /cardano-node/cardano-api/src/Cardano/Api/Script.hs::1061
+  let hashes =  map hashScript scripts' :: [ScriptHash]
 
   -- then generate an TxIns which spends funds from these scripts
-  txIns <- undefined
+  txIns <- do
+    txIns' <- replicateM nScripts $ forAll $ CGen.genTxIn
+    let zipped = zip hashes txIns'
+    return $ map (\(hash, TxIn _ ix) -> TxIn (TxId undefined) ix) zipped
+
+  -- txIns <- undefined
   -- type TxIns build era = [(TxIn, BuildTxWith build (Witness WitCtxTxIn era))]
   -- ^ /cardano-node/cardano-api/src/Cardano/Api/TxBody.hs::1157
 
   -- then generate a transaction with this TxIns (based on `genTxBodyContent`)
+--  txBodyContent <- CGen.genTxBodyContent
+
+
   -- genTxBodyContent
   -- data TxBodyContent build era =
   -- ^ /cardano-node/cardano-api/src/Cardano/Api/TxBody.hs::1518
